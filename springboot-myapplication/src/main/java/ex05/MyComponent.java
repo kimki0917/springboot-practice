a409806665c1fe385d3e563b68e6c9440e6db896
kimki0917 <kimki0917@naver.com>
2023-02-15 19:38:29 +0900

    asdf

---
+++ springboot-myapplication/src/main/java/ex05/MyComponent.java
@@ -0,0 +1,9 @@
+package ex05;
+
+/**
+ * ApplicationRunner 인터페이스의 구현 클래스 빈(HelloWorldRunner) 사용하기
+ * 
+ * */
+public class MyComponent {
+
+}
---
+++ springboot-myapplication/src/main/java/ex05/component/MyComponent.java
@@ -0,0 +1,5 @@
+package ex05.component;
+
+public class MyComponent {
+
+}
